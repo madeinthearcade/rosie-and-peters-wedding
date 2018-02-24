6223d9fbe8bc650f4682db67557aaf7155b560b2
Peter Chan <peterchan@peters-mbp.home>
2018-02-24 23:03:04 +0000

    hero, image links

---
+++ wp-content/themes/wedding-theme/partials/imagelinks.php
@@ -0,0 +1,38 @@
+<section id="imagelinks" class="flex">
+   <div class="flex__item" id="destination">
+      <img src="<?php echo get_template_directory_uri(); ?>/dist/img/template/destination-bg.png" alt="Destination">
+      <div class="flex__caption">
+         <h3>Destination</h3>
+         <p>Santorini, Greece</p>
+         <p>
+            <a href="#" class="btn" target="_blank">
+               Find Flights
+            </a>
+         </p>
+      </div>
+   </div>
+   <div class="flex__item" id="date">
+      <img src="<?php echo get_template_directory_uri(); ?>/dist/img/template/date-bg.png" alt="Date">
+      <div class="flex__caption">
+         <h3>Date</h3>
+         <p>Saturday 21st July 2018</p>
+         <p>
+            <a href="#" class="btn" target="_blank">
+               RSVP
+            </a>
+         </p>
+      </div>
+   </div>
+   <div class="flex__item" id="venue">
+      <img src="<?php echo get_template_directory_uri(); ?>/dist/img/template/venue-bg.png" alt="Venue">
+      <div class="flex__caption">
+         <h3>Venue</h3>
+         <p>Venetsanos Winery</p>
+         <p>
+            <a href="#" class="btn" target="_blank">
+               Directions
+            </a>
+         </p>
+      </div>
+   </div>
+</section>
