3f700ae7ba1b8fac8f6e0bbe05ef9a70ed09167c
Peter Chan <peterchan@peters-mbp.home>
2018-02-24 19:16:14 +0000

    embed google fonts

--- wp-content/themes/wedding-theme/functions.php
+++ wp-content/themes/wedding-theme/functions.php
@@ -110,8 +110,13 @@ function html5blank_conditional_scripts()
 // Load HTML5 Blank styles
 function html5blank_styles()
 {
+
+	wp_register_style('googlefonts', 'https://fonts.googleapis.com/css?family=Josefin+Sans:700|Yeseva+One', array(), '', 'all');
+	wp_enqueue_style('googlefonts');
+
 	wp_register_style('style', get_template_directory_uri() . '/dist/css/style.min.css', array(), '1.0', 'all');
 	wp_enqueue_style('style');
+
 }
 
 // Register HTML5 Blank Navigation
@@ -395,34 +400,34 @@ register_taxonomy_for_object_type('post_tag', 'html5-blank'); // Rename to match
 register_post_type('html5-blank', // Register Custom Post Type
 	array(
 		'labels' => array(
-'name' => __('Custom Post', 'html5blank'), // Rename these to suit
-'singular_name' => __('Custom Post', 'html5blank'),
-'add_new' => __('Add New', 'html5blank'),
-'add_new_item' => __('Add New Custom Post', 'html5blank'),
-'edit' => __('Edit', 'html5blank'),
-'edit_item' => __('Edit Custom Post', 'html5blank'),
-'new_item' => __('New Custom Post', 'html5blank'),
-'view' => __('View Custom Post', 'html5blank'),
-'view_item' => __('View Custom Post', 'html5blank'),
-'search_items' => __('Search Custom Post', 'html5blank'),
-'not_found' => __('No Custom Posts found', 'html5blank'),
-'not_found_in_trash' => __('No Custom Posts found in Trash', 'html5blank')
-),
+			'name' => __('Custom Post', 'html5blank'), // Rename these to suit
+			'singular_name' => __('Custom Post', 'html5blank'),
+			'add_new' => __('Add New', 'html5blank'),
+			'add_new_item' => __('Add New Custom Post', 'html5blank'),
+			'edit' => __('Edit', 'html5blank'),
+			'edit_item' => __('Edit Custom Post', 'html5blank'),
+			'new_item' => __('New Custom Post', 'html5blank'),
+			'view' => __('View Custom Post', 'html5blank'),
+			'view_item' => __('View Custom Post', 'html5blank'),
+			'search_items' => __('Search Custom Post', 'html5blank'),
+			'not_found' => __('No Custom Posts found', 'html5blank'),
+			'not_found_in_trash' => __('No Custom Posts found in Trash', 'html5blank')
+		),
 		'public' => true,
-'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
-'has_archive' => true,
-'supports' => array(
-	'title',
-	'editor',
-	'excerpt',
-	'thumbnail'
-), // Go to Dashboard Custom HTML5 Blank post for supports
-'can_export' => true, // Allows export in Tools > Export
-'taxonomies' => array(
-	'post_tag',
-	'category'
-) // Add Category and Post Tags support
-));
+		'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
+		'has_archive' => true,
+		'supports' => array(
+			'title',
+			'editor',
+			'excerpt',
+			'thumbnail'
+		), // Go to Dashboard Custom HTML5 Blank post for supports
+		'can_export' => true, // Allows export in Tools > Export
+		'taxonomies' => array(
+			'post_tag',
+			'category'
+		) // Add Category and Post Tags support
+	));
 }
 
 /*------------------------------------*\
