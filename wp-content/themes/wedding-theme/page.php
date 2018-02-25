a5f57e49dfd08815873723896bc61f825ea7e55b
Peter Chan <peterchan@peters-mbp.home>
2018-02-25 14:39:01 +0000

    schedule sorted

--- wp-content/themes/wedding-theme/page.php
+++ wp-content/themes/wedding-theme/page.php
@@ -3,6 +3,7 @@
    <?php 
    get_template_part('partials/hero'); 
    get_template_part('partials/imagelinks');
+   get_template_part('partials/schedule');
    ?>
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/wedding-theme/partials/schedule.php
@@ -0,0 +1,83 @@
+<section id="schedule" class="section">
+   <div class="container">
+      <h2 class="text-center">Schedule</h2>
+      <div class="row">
+         <div class="col-sm-10 col-sm-offset-1">
+
+            <div class="schedule__item">
+               <div class="schedule__item__left">
+                  <h3>Bridal games</h3>
+                  <p>
+                     Family and Bridal Party - Ladies Only
+                     <br>
+                     Dress: Casual
+                  </p>
+               </div>
+               <div class="schedule__item__right">
+                  <p>
+                     9:30 - 10:00AM
+                     <br>
+                     Anemomilos Hotel
+                  </p>
+               </div>
+            </div>
+
+            <div class="schedule__item">
+               <div class="schedule__item__left">
+                  <h3>Bridal games</h3>
+                  <p>
+                     Family and Bridal Party - Ladies Only
+                     <br>
+                     Dress: Casual
+                  </p>
+               </div>
+               <div class="schedule__item__right">
+                  <p>
+                     9:30 - 10:00AM
+                     <br>
+                     Anemomilos Hotel
+                  </p>
+               </div>
+            </div>
+
+            <div class="schedule__item">
+               <div class="schedule__item__left">
+                  <h3>Bridal games</h3>
+                  <p>
+                     Family and Bridal Party - Ladies Only
+                     <br>
+                     Dress: Casual
+                  </p>
+               </div>
+               <div class="schedule__item__right">
+                  <p>
+                     9:30 - 10:00AM
+                     <br>
+                     Anemomilos Hotel
+                  </p>
+               </div>
+            </div>
+
+            <div class="schedule__item">
+               <div class="schedule__item__left">
+                  <h3>Bridal games</h3>
+                  <p>
+                     Family and Bridal Party - Ladies Only
+                     <br>
+                     Dress: Casual
+                  </p>
+               </div>
+               <div class="schedule__item__right">
+                  <p>
+                     9:30 - 10:00AM
+                     <br>
+                     Anemomilos Hotel
+                  </p>
+               </div>
+            </div>
+
+            <!-- /col-sm-10 -->
+         </div>
+      </div>
+   </div>
+</section>
